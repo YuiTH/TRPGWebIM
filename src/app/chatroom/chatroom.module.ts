45006fe1affc1390f0148ac7e98dc01266b24f42
YuiTH <396698132@qq.com>
2018-09-07 10:51:48 +0800

    Init chatroom moudle and message box.

---
+++ src/app/chatroom/chatroom.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessageInputBoxComponent } from './message-input-box/message-input-box.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MessageInputBoxComponent]
+})
+export class ChatroomModule { }
---
+++ src/app/chatroom/message-input-box/message-input-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-input-box',
+  templateUrl: './message-input-box.component.html',
+  styleUrls: ['./message-input-box.component.css']
+})
+export class MessageInputBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
