0692d0c605b9d8482b6b22a4c8f786dd475c16d0
YuiTH <396698132@qq.com>
2018-09-12 12:06:08 +0800

    Angular material test.

--- src/app/chatroom/chatroom.module.ts
+++ src/app/chatroom/chatroom.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessageInputBoxComponent } from './message-input-box/message-input-box.component';
 import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   exports: [
     MessageInputBoxComponent,
