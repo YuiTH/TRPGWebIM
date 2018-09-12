db39c3cd782ac6a5bc1864b2f8bed06d07b36a44
YuiTH <396698132@qq.com>
2018-09-12 10:39:36 +0800

    Add angular material module. Fix dependency problem.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {ChatroomModule} from './chatroom/chatroom.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {ChatroomModule} from './chatroom/chatroom.module';
     BrowserModule,
     FormsModule,
     ChatroomModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
