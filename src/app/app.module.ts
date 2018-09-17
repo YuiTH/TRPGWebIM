82133c2cbb79ca8a11f8c46194cede96f08ea208
YuiTH <396698132@qq.com>
2018-09-17 23:39:28 +0800

    Try to add router.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {ChatroomModule} from './chatroom/chatroom.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LogDisplayComponent } from './log-displayer/log-display.component';
 import {MatButtonModule} from '@angular/material';
+import {Route, RouterModule} from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -19,6 +20,11 @@ import {MatButtonModule} from '@angular/material';
     ChatroomModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
